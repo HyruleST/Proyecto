c8cad1ce6d99e65396cecf7026c926634155e5ae
Esteban <104602184+HyruleST@users.noreply.github.com>
2024-03-21 19:08:46 -0600

    Add files via upload

---
+++ main.js
@@ -0,0 +1,21 @@
+const flower = document.querySelector("#complete_flower");
+const secondPage = document.querySelector("#second_part");
+const text = document.querySelector(".text");
+const button = document.querySelector('#reset');
+
+
+
+flower.addEventListener("click", () => {
+  flower.classList.add("flower_animated");
+  setTimeout(() => {
+    window.location = "#second_part";
+  }, 500);
+  setTimeout(() => {
+    secondPage.classList.add("animated_back");
+  }, 600);
+});
+
+
+button.addEventListener("click",()=>{
+  window.location = "/";
+});
